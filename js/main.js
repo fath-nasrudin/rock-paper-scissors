13a3ec429b2d2e6812c6efff3b41fc8447b2bae2
fath-nasrudin <fath.nasrudin.i@gmail.com>
2023-01-30 11:39:13 +0700

    feat: Add basic functionality

--- js/main.js
+++ js/main.js
@@ -15,27 +15,33 @@ const checkValidValue = (input) => {
   return (input === 'paper' || input === 'rock' || input === 'scissors')
 }
 
+// return the winner;
+// input: computer and user selection
+// output: comp/user/draw/-1;
 const compareSelections = (computerSelection, userSelection) => {
-  let message = 'Something went wrong.';
+  const user = 'user';
+  const comp = 'comp';
+  const draw = 'draw';
+  const err = -1;
 
   // draw
-  if (computerSelection === userSelection) message = `Draw! both player have chosen ${computerSelection}`;
+  if (computerSelection === userSelection) return draw;
 
   if (computerSelection === 'rock') {
-    if (userSelection === 'paper') message = 'You win! paper beats rock'
-    if (userSelection === 'scissors') message = 'You Lose! rock beats scissors'
+    if (userSelection === 'paper') return user;
+    if (userSelection === 'scissors') return comp;
   } 
 
   if (computerSelection === 'paper') {
-    if (userSelection === 'rock') message = 'You Lose! paper beats rock'
-    if (userSelection === 'scissors') message = 'You Win! scissors beats paper'
+    if (userSelection === 'rock') return comp;
+    if (userSelection === 'scissors') return user;
   } 
 
   if (computerSelection === 'scissors') {
-    if (userSelection === 'rock') message = 'You win! rock beats scissors'
-    if (userSelection === 'paper') message = 'You Lose! scissors beats paper'
+    if (userSelection === 'rock') return user;
+    if (userSelection === 'paper') return comp;
   } 
-  return message;
+  return err;
 }
 
 const playRound = (computerSelection, userSelection) => {
@@ -60,3 +66,149 @@ const playGame = (round=5) => {
 }
 
 // playGame()
+
+
+
+// check score
+// input: maximal score (number);
+// output: boolean
+const checkMaxScore = (maxScore = 5) => {
+  const userScore = Number(document.querySelector('#user-score-number').textContent);
+  const computerScore = Number(document.querySelector('#computer-score-number').textContent);
+
+  return (userScore >= maxScore || computerScore >= maxScore);
+}
+
+// return: string -> comp/user/draw;
+const getWinner = () => {
+  const userScore = Number(document.querySelector('#user-score-number').textContent);
+  const computerScore = Number(document.querySelector('#computer-score-number').textContent);
+
+  if (userScore > computerScore) return 'user';
+  if (userScore < computerScore) return 'comp';
+  if (userScore === computerScore) return 'draw';
+  return -1;
+}
+
+// update ui score
+// input winner name: user/comp/draw/
+const updateScoreUI = (winner) => {
+  if (winner === 'draw') return;
+
+  // access computer and user score
+  if (winner === 'comp') {
+    const computerScoreNumber = document.querySelector('#computer-score-number');
+    const score = Number(computerScoreNumber.textContent);
+    computerScoreNumber.textContent = score + 1;
+    return;
+  }
+
+  if (winner === 'user') {
+    const userScoreNumber = document.querySelector('#user-score-number');
+    const score = Number(userScoreNumber.textContent);
+    userScoreNumber.textContent = score + 1;
+    return;
+  }
+}
+
+// update log 
+// input text string
+const addLogUI = (text) => {
+  const battleLog = document.querySelector('.battle-log');
+  const p = document.createElement('p');
+  p.textContent = text;
+  battleLog.appendChild(p);
+}
+
+// winner: string -> comp/user;
+// do: update ui to add the winner;
+const addWinnerUI = (winner) => {
+  const scoreSection = document.querySelector('.content.score');
+  const resultSection = document.createElement('div');
+  const resultText = document.createElement('h2');
+
+  let text = '';
+  if(winner === 'user') text = 'Congrats. You Win!!';
+  if(winner === 'comp') text = 'Unfortunately, You lose :(';
+  resultText.textContent = text;
+
+  resultSection.appendChild(resultText);
+  scoreSection.after(resultSection);
+} 
+
+const addPlayAgainButtonUI = () => {
+  const buttonChoiceSection = document.querySelector('.button-choice');
+  const link = document.createElement('a');
+  link.setAttribute('href', '/'); 
+
+  const playAgainButton = document.createElement('button');
+  playAgainButton.textContent = 'Play Again';
+
+  link.appendChild(playAgainButton);
+  buttonChoiceSection.before(link);
+}
+
+const disableSelectionButtonUI = () => {
+  const buttonSelection = document.querySelector('#button-selection');
+Array
+  .from(buttonSelection.children)
+  .forEach(button => {
+    button.setAttribute('disabled', '')
+  })
+  console.log('button disabled')
+}
+
+
+
+// play 1 round based on user click
+const playByClick = (e) => {
+  // define selection
+  const userSelection = e.target.value;
+  const computerSelection = getComputerChoice();
+
+  const roundWinner = compareSelections(computerSelection, userSelection);
+  if (roundWinner === -1) console.log('Error: Something went wrong!')
+  updateScoreUI(roundWinner);
+
+  // add log
+  let text = '';
+  if (roundWinner === 'draw') {
+    text = `draw: user and computer use ${userSelection}`
+  } else
+  if (roundWinner !== 'draw') {
+    text = `${roundWinner} +1: ${userSelection} ${(roundWinner=== 'user')? 'beats' : 'beaten by'} ${computerSelection}`;
+  }
+  addLogUI(text);
+
+  const maxScore = checkMaxScore(5);
+  if (maxScore) {
+    const winner = getWinner();
+    addWinnerUI(winner);
+    addPlayAgainButtonUI();
+    disableSelectionButtonUI();
+  }
+}
+
+// access button
+const buttonSelection = document.querySelector('#button-selection');
+Array
+  .from(buttonSelection.children)
+  .forEach(button => {
+    button.addEventListener('click', playByClick);
+  })
+
+// when click play round
+
+// get user input
+// get computer input
+
+// compare
+// send 
+// 
+
+// when one of the score is 5, end the game.
+// test
+
+// add play again button;
+// add restart button;
+// console.log(compareSelections('paper', 'rock'))
